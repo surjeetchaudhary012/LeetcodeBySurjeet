c3c0f8bb00c9d69cb43d9a2fbe5d95252839d112
surjeetchaudhary012 <103649977+surjeetchaudhary012@users.noreply.github.com>
2022-11-10 00:11:41 +0530

    Time: 31 ms (45.40%), Space: 50.4 MB (58.31%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -0,0 +1,60 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        
+        int [] ans  = new int[k];
+        
+        HashMap<Integer,Integer> map =  new HashMap<>();
+        
+        for(int i = 0; i < nums.length; i++){
+            map.put(nums[i],map.getOrDefault(nums[i],0) + 1);
+        }
+       
+        
+        
+        PriorityQueue<Pair> pq = new PriorityQueue<>(
+            
+            (a,b) -> {
+            return a.frequency - b.frequency;
+        });
+        
+        for(Integer val : map.keySet()){
+            
+            if(pq.size() < k){
+                pq.add(new Pair(val,map.get(val)));
+            } else {
+                if(map.get(val) > pq.peek().frequency){
+                    pq.remove();
+                     pq.add(new Pair(val,map.get(val)));
+                }
+            }
+            
+             // System.out.println(pq);
+            
+        }
+        
+        
+        
+        int i = 0;
+        
+        while(pq.size() > 0){
+            ans[i] = pq.peek().element;
+            pq.remove();
+            i++;
+        }
+        
+        return ans;
+    }
+
+}
+
+class Pair {
+    
+    int element;
+    int frequency;
+    
+    Pair(int element , int frequency){
+        this.element = element;
+        this.frequency = frequency;
+    }
+    
+}
