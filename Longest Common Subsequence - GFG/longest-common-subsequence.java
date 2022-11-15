9c73333e05bea7574b1937b0084d8151e619c2c9
surjeetchaudhary012 <103649977+surjeetchaudhary012@users.noreply.github.com>
2022-11-15 23:08:07 +0530

    Added solution - LeetHub

--- Longest Common Subsequence - GFG/longest-common-subsequence.java
+++ Longest Common Subsequence - GFG/longest-common-subsequence.java
@@ -38,28 +38,16 @@ class Solution
         for(int i = 1; i < dp.length; i++){
             for(int j = 1; j < dp[0].length; j++){
                 
-                    int take = 0;
-                    int notTake = 0;
-                    
                     if(s1.charAt(i-1) == s2.charAt(j-1)){
                         dp[i][j] = 1 + dp[i-1][j-1];
                     } else {
                        dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
                     }
-                    
-                    // dp[i][j] = Math.max(take,notTake);
                 
             }
         }
         
         return dp[n][m];
-        
-        // for(int [] arr : dp){
-        //     Arrays.fill(arr,-1);
-        // }
-        
-        // return findLCS(s1,s2,n-1,m-1,dp);
-        
         
     }
     
