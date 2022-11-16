85c5fe17bdaa7ddeec9c944ba1355a10380118e4
surjeetchaudhary012 <103649977+surjeetchaudhary012@users.noreply.github.com>
2022-11-16 23:46:22 +0530

    Added solution - LeetHub

---
+++ Longest Increasing Subsequence - GFG/longest-increasing-subsequence.java
@@ -0,0 +1,75 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*; 
+
+class GFG{
+    public static void main(String args[]) throws IOException { 
+        
+        //taking input using Scanner class
+        Scanner sc = new Scanner(System.in);
+        
+        //taking total testcases
+        int t = sc.nextInt();
+        while(t > 0){
+            
+            //taking size of array
+            int n = sc.nextInt();
+            int array[] = new int[n];
+            
+            //inserting elements in the array
+            for (int i = 0; i < n; ++i)
+            {
+                array[i] = sc.nextInt();
+            }
+            
+            //creating an object of class Solution
+            Solution ob = new Solution();
+            
+            //calling longestSubsequence() method of class
+            //Solution
+            System.out.println(ob.longestSubsequence(n,array));
+            t--;
+        }
+    } 
+} 
+// } Driver Code Ends
+
+
+
+
+class Solution 
+{
+    //Function to find length of longest increasing subsequence.
+    static int longestSubsequence(int size, int arr[])
+    {
+        int n = arr.length;
+        int [][] dp = new int[n][n+1];
+        
+        for(int [] a : dp){
+            Arrays.fill(a,-1);
+        }
+        
+        return findLIS(arr,0,-1,dp);
+        
+    }
+    
+    private static int findLIS(int [] arr, int idx , int prev , int [][] dp){
+        
+        if(idx == arr.length){
+            return 0;
+        }
+        
+        if(dp[idx][prev + 1] != -1){
+            return dp[idx][prev+1];
+        }
+        int take = 0;
+        
+        if(prev == -1 || arr[idx] > arr[prev]){
+            take = 1 + findLIS(arr,idx + 1,idx,dp);
+        }
+        
+      int  notTake = findLIS(arr,idx+1,prev,dp);
+        
+        return dp[idx][prev+1] = Math.max(take ,notTake);
+    }
+} 
