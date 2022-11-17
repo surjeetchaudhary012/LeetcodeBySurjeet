f2a82c7a88082fe655bf72bd4f791a5c9f9123e2
surjeetchaudhary012 <103649977+surjeetchaudhary012@users.noreply.github.com>
2022-11-18 00:45:45 +0530

    Time: 2 ms (87.99%), Space: 51.8 MB (73.53%) - LeetHub

--- 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.java
+++ 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.java
@@ -16,10 +16,10 @@
 class Solution {
     public int sumEvenGrandparent(TreeNode root) {
         
-        return dfs(root);
+        return dfs(root,-1,-1);
     }
     
-    private int dfs(TreeNode root){
+    private int dfs(TreeNode root,int parent,int grandParent){
         
         if(root == null){
             return 0;
@@ -27,35 +27,10 @@ class Solution {
         
         int sum = 0;
         
-        if(root.val % 2 == 0){
-            
-            if(root.left != null){
-                
-                  if(root.left.left != null){
-                sum += root.left.left.val;
-            }
-            
-            if(root.left.right != null){
-                sum += root.left.right.val;
-            }
-                
-            }
-            
-            if(root.right != null){
-                
-                 if(root.right.left != null){
-                sum += root.right.left.val;
-            }
-            
-            if(root.right.right != null){
-                sum += root.right.right.val;
-            }
-                
-            }   
-           
-            
+        if(grandParent % 2 == 0){
+            sum = root.val;
         }
         
-        return sum + dfs(root.left) + dfs(root.right);
+        return sum + dfs(root.left,root.val,parent) + dfs(root.right,root.val,parent);
     }
 }
