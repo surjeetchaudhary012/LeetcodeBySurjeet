6c15bb8479aa1f2a7a6158125bff39cbd92d4467
surjeetchaudhary012 <103649977+surjeetchaudhary012@users.noreply.github.com>
2022-11-19 15:36:12 +0530

    Time: 5 ms (30.36%), Space: 42.5 MB (74.73%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,10 +1,12 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
         
-      int n = grid.length;
+            int n = grid.length;
         int m = grid[0].length;
     
         int [][] visited = new int[n][m];
+        
+        int freshCount  = 0;
         
         Queue<Pair> que = new ArrayDeque<>();
         
@@ -15,10 +17,16 @@ class Solution {
                     que.add(new Pair(i,j,0));
                     visited[i][j] = 1;
                 } 
+                
+                if(grid[i][j] == 1){
+                    freshCount++;
+                }
+                
             }
         }
         
         int minTime = 0;
+        int rottedCount = 0;
         
         int [] rdr = {-1,0,1,0};
         int [] cdr = {0,1,0,-1};
@@ -33,14 +41,18 @@ class Solution {
             
             minTime = Math.max(time,minTime);
             
+            if(grid[row][col] == 1){
+                rottedCount++;
+            }
+            
             for(int i = 0; i < 4; i++){
                 
                 int r = row + rdr[i];
                 int c = col + cdr[i];
                 int t = time + 1;
                 
-                if(r < 0 || r == n || c < 0 || c == m || grid[r][c] != 1 || visited[r][c] == 1){
-        
+              if(r < 0 || r == n || c < 0 || c == m || grid[r][c] != 1 || visited[r][c] == 1){
+                    continue;
                 } else {
                     
                     que.add(new Pair(r,c,t));
@@ -50,15 +62,7 @@ class Solution {
             }
         }
         
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(visited[i][j] == 0 && grid[i][j] == 1){
-                    return -1;
-                }
-            }
-        }
-        
-        return minTime;
+        return rottedCount == freshCount ? minTime : -1;
     }
 }
 
