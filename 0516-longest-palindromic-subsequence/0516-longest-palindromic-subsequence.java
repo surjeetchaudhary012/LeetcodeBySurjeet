988b1031b2506f5f4296caba62346b9693ec8d90
surjeetchaudhary012 <103649977+surjeetchaudhary012@users.noreply.github.com>
2022-11-25 22:58:10 +0530

    Time: 61 ms (83.65%), Space: 68.6 MB (63.27%) - LeetHub

---
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int longestPalindromeSubseq(String s) {
+        
+       int n = s.length();
+       int [][] dp = new int[n][n];
+       
+       for(int [] a : dp){
+           Arrays.fill(a,-1);
+       }
+       
+       return  findLPS(s,0,n-1,dp);
+    }
+    
+    private int findLPS(String s , int start, int end, int [][] dp){
+        
+        if(start > end){
+            return 0;
+        }
+        
+        if(start == end){
+            return 1;
+        }
+        
+        if(dp[start][end] != -1){
+            return dp[start][end];
+        }
+        
+        int match = 0;
+        int notMatch = 0;
+        
+        if(s.charAt(start) == s.charAt(end)){
+            match = 2 + findLPS(s,start + 1,end - 1,dp);
+        } else {
+            notMatch = Math.max(findLPS(s,start + 1,end,dp),findLPS(s,start,end - 1,dp));
+        }
+        
+        return dp[start][end] =  Math.max(match,notMatch);
+    }
+}
