68364d9ce73fb635893e6bc0d02fc847d48c8a71
surjeetchaudhary012 <103649977+surjeetchaudhary012@users.noreply.github.com>
2022-11-27 00:31:25 +0530

    Time: 20 ms (81.14%), Space: 42.8 MB (87.78%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -6,30 +6,29 @@ class Solution {
         
         int mod = 1000000000  + 7;
 	  
-	  int [][] dp = new int[n+1][m+1];
-        
-        for(int i = 0; i < n; i++){
-            dp[i][0]  = 1;
-        }
+	 int [] prev = new int[m+1];
+        prev[0] = 1;
         
         for(int i = 1; i <= n; i++){
+            
+            int [] curr = new int[m+1];
+            
+            curr[0] = 1;
+            
             for(int j = 1; j <= m; j++){
                 
-                int match = 0;
-                int notMatch = 0;
-                
                 if(s.charAt(i-1) == t.charAt(j-1)){
-                     match  = (dp[i-1][j-1] + dp[i-1][j]);
-                }  else {
-                    notMatch = dp[i-1][j] %  mod;
+                    curr[j] = (prev[j-1] + prev[j]) % mod;
+                } else {
+                    curr[j] = prev[j];
                 }
                 
-                dp[i][j] = (match + notMatch) % mod;
             }
-        }
-        
-    return dp[n][m];
-	  
+            
+            prev = curr;
+            
+        }    
+        return prev[m];
     }
 }
     
