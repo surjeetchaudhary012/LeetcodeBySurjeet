0b2a72e125d79f6850392ecb938b86918a188c12
surjeetchaudhary012 <103649977+surjeetchaudhary012@users.noreply.github.com>
2022-11-27 01:55:23 +0530

    Time: 7 ms (81.69%), Space: 44.8 MB (62.21%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -4,30 +4,33 @@ class Solution {
         int n = word1.length();
         int m = word2.length();
         
-        int [][] dp = new int[n+1][m+1];
+        int [] dp = new int[m+1];
         
-        for(int i = 0; i <= n; i++){
-            for(int j = 0; j <= m; j++){
+        for(int j = 0; j <= m; j++){
+            dp[j] = j;
+        }
+        
+        for(int i = 1; i <= n; i++){
+            
+            int [] curr = new int[m + 1];
+            
+            curr[0] = i;
+            
+            for(int j = 1; j <= m; j++){
                 
-                if(i == 0){
-                    dp[i][j] =  j;
-                }
-                
-              else  if(j == 0){
-                    dp[i][j] = i;
-                }
-                
-               else  if(word1.charAt(i-1) == word2.charAt(j-1)){
-                    dp[i][j] = dp[i-1][j-1];
+               if(word1.charAt(i-1) == word2.charAt(j-1)){
+                    curr[j] =  dp[j-1];
                 } else {
                     
-                   dp[i][j] = 1 + Math.min(dp[i-1][j],Math.min(dp[i-1][j-1],dp[i][j-1]));
+                   curr[j] = 1 + Math.min(dp[j],Math.min(dp[j-1],curr[j-1]));
                 }
                 
             }
+            
+            dp = curr;
         }
-        
-        return dp[n][m];
+         
+        return dp[m];
         
     }
     
