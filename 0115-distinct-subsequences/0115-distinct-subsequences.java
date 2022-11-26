b18eae235d2936a7b594867b3e9a5c4b4c253e74
surjeetchaudhary012 <103649977+surjeetchaudhary012@users.noreply.github.com>
2022-11-27 00:24:35 +0530

    Time: 50 ms (9.80%), Space: 50.5 MB (19.90%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,50 +1,36 @@
 class Solution {
     public int numDistinct(String s, String t) {
         
-    int n = s.length();
+      int n = s.length();
 	  int m = t.length();
-	  
-	  int [][] dp = new int[n][m];
-	  
-	  for(int [] arr : dp){
-	      Arrays.fill(arr,-1);
-	  }
-	  
-	return   find(s,t,0,0,dp);
-	  
-    }
-    
-    private int find(String s , String t , int i , int j,int [][] dp){
         
-                if(j == t.length()){
-            return 1;
+        int mod = 1000000000  + 7;
+	  
+	  int [][] dp = new int[n+1][m+1];
+        
+        for(int i = 0; i < n; i++){
+            dp[i][0]  = 1;
         }
         
-        if(i == s.length()){
-          return 0;
-            } 
-        
-        int mod = 1000000000 + 7;
-        
-
-        
-        if(dp[i][j] != -1){
-            return dp[i][j];
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= m; j++){
+                
+                int match = 0;
+                int notMatch = 0;
+                
+                if(s.charAt(i-1) == t.charAt(j-1)){
+                     match  = (dp[i-1][j-1] + dp[i-1][j]);
+                }  else {
+                    notMatch = dp[i-1][j] %  mod;
+                }
+                
+                dp[i][j] = (match + notMatch) % mod;
+            }
         }
         
-        int match = 0;
-        int notMatch = 0;
-        
-        if(s.charAt(i) == t.charAt(j)){
-            
-         match =  find(s,t,i+1,j+1,dp) + find(s,t,i+1,j,dp);
-        
-        } else {
-            
-            notMatch = find(s,t,i+1,j,dp);
-        }
-        
-        return dp[i][j] = (match + notMatch) % mod;
-        
+    return dp[n][m];
+	  
     }
 }
+    
+   
