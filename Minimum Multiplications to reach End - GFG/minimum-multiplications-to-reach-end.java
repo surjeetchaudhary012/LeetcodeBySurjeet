bab50dbb7cccb2c1e277b3a3acaca29ec0f48e6d
surjeetchaudhary012 <103649977+surjeetchaudhary012@users.noreply.github.com>
2022-12-06 23:01:28 +0530

    Added solution - LeetHub

---
+++ Minimum Multiplications to reach End - GFG/minimum-multiplications-to-reach-end.java
@@ -0,0 +1,95 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// Position this line where user code will be pasted.
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int n = sc.nextInt();
+            int[] a = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                a[i] = sc.nextInt();
+            }
+
+            int start = sc.nextInt();
+            int end = sc.nextInt();
+
+            Solution ob = new Solution();
+            int ans = ob.minimumMultiplications(a, start, end);
+            System.out.println(ans);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    int minimumMultiplications(int[] arr, int start, int end) {
+        
+        PriorityQueue<Node> que = new PriorityQueue<>((a,b) -> {
+            return Integer.compare(a.count,b.count);
+        });
+        
+        HashSet<Integer> set = new HashSet<>();
+        
+        
+        int mod = 100000;
+        
+        que.add(new Node(start,0));
+        set.add(start);
+        
+        while(que.size() > 0){
+            
+            Node node = que.remove();
+            int val = node.val;
+            int count = node.count;
+            
+            for(int i = 0; i < arr.length; i++){
+                
+                int num = val * arr[i];
+                
+                num = num % mod;
+                
+                 if(num == end){
+                     return count + 1;
+                 }
+                 
+                //  System.out.println(set + "-->" + num);
+               
+                 if(set.contains(num)){
+                     continue;
+                 }
+                 
+                 if(set.size() >= mod){
+                     return -1;
+                 }
+                 
+                 set.add(num);
+                 que.add(new Node(num,count + 1));
+                
+            }
+        }
+ 
+      return -1;
+    }
+}
+
+class Node {
+    int val;
+    int count;
+    
+    Node(int val , int count){
+        this.val = val;
+        this.count = count;
+    }
+}
